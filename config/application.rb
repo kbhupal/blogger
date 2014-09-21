89dda7a14b6106116f20f7463e15c308dbe53170
Shanil Puri <spuri3@ncsu.edu>
2014-09-20 22:39:01 -0400

    Corrected the views to be generated correctly.

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,21 @@ module Blogger
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # config.assets.compile = true
+    # config.assets.precompile +=  %w(*.png *.jpg *.jpeg *.gif*.js, *.css, *.css.erb, *.sass)
+    initializer 'setup_asset_pipeline', :group => :all  do |app|
+      # We don't want the default of everything that isn't js or css, because it pulls too many things in
+      app.config.assets.precompile.shift
+
+      # Explicitly register the extensions we are interested in compiling
+      app.config.assets.precompile.push(Proc.new do |path|
+        File.extname(path).in? [
+                                   '.html', '.erb', '.haml',                 # Templates
+                                   '.png',  '.gif', '.jpg', '.jpeg',         # Images
+                                   '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+                               ]
+      end)
+    end
   end
 end
