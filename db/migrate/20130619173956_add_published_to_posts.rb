28495c15d32bdb340299c4eb5d259b05a673c926
James Beavers <jamesjbeavers@gmail.com>
2014-09-21 04:49:44 -0400

    Delete some old migrations, update others.  Also added new migrations

--- db/migrate/20130619173956_add_published_to_posts.rb
+++
@@ -1,5 +0,0 @@
-class AddPublishedToPosts < ActiveRecord::Migration
-  def change
-    add_column :posts, :draft, :boolean
-  end
-end
--- db/migrate/20140921014021_devise_create_users.rb
+++ db/migrate/20140921014021_devise_create_users.rb
@@ -1,6 +1,6 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
-    create_table(:users) do |t|
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              :null => false, :default => ""
       t.string :encrypted_password, :null => false, :default => ""
@@ -20,6 +20,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string   :last_sign_in_ip
 
       t.timestamps
+
+      t.string  :phone
     end
 
     add_index :users, :email,                :unique => true
