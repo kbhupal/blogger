25fec206a1524ddd8f840606d5908dc4a0c1b1f1
James Beavers <jamesjbeavers@gmail.com>
2014-09-21 03:10:14 -0400

    Add new table migrations for tags, jobs, resumes, categories, skills

---
+++ db/migrate/20140921061139_create_jobs.rb
@@ -0,0 +1,17 @@
+class CreateJobs < ActiveRecord::Migration
+  def up
+    create_table :jobs do |t|
+      t.integer   :owner_id
+      t.string    :title
+      t.string    :description
+      t.integer   :salary
+      t.string    :location
+      t.integer   :category_id
+      t.datetime  :deadline
+    end
+  end
+
+  def down
+    drop_table    :jobs
+  end
+end
